b17c2bf521db20cf140503bb6eb3562b83726611
BenoitEirik <olafsergent@gmail.com>
2023-06-28 00:17:48 +0200

    Add icons phosphor from Iconify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/supabase',
     '@nuxtjs/i18n',
+    'nuxt-icon'
   ],
   app: {
     head: {
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,8 +1,22 @@
 <template>
+<div>
+  <NuxtLink to="/">Back</NuxtLink>
+  <h1>Playground</h1>
+
+  <h2>Icons Phosphor collection</h2>
+  <ul>
+    <li><a href="https://icones.js.org/collection/ph" target="_blank">Phosphor</a></li>
+    <li><a href="https://nuxt.com/modules/icon" target="_blank">Nuxt module</a></li>
+  </ul>
   <div>
-    <NuxtLink to="/">Back</NuxtLink>
-    <h1>Playground</h1>
+    <Icon name="ph:play" color="#888888" />
+    <Icon name="ph:airplay" color="#888888" />
+    <Icon name="ph:pause" color="#888888" />
+    <Icon name="ph:stop" color="#888888" />
+    <Icon name="ph:medal-military" color="#888888" />
+    <Icon name="ph:bell" color="#888888" />
   </div>
+</div>
 </template>
 
 <script setup lang="ts">
