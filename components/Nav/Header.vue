14d05f4d27c31151e7cac94ec30c3abf29908a27
BenoitEirik <olafsergent@gmail.com>
2023-07-08 17:54:49 +0200

    Add header

---
+++ components/Nav/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="sticky flex items-center drop-shadow-md border-b">
+  <div class="p-4">
+    <img src="/android-chrome-512x512.png" class="w-10">
+  </div>
+  <NuxtLink to="/" class="vagg-logo text-xl">Vagg</NuxtLink>
+  <div class="grow-1 h-auto" />
+  <div class="p-4">
+    <LibBurger />
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.vagg-logo {
+  font-family: 'Raleway';
+  color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <form>
-      <select v-model="locale">
-        <option value="en">en</option>
-        <option value="fr">fr</option>
-      </select>
-      <p>{{ $t("welcome") }}</p>
-      <p>{{ $t("description") }}</p>
-    </form>
-    <NuxtLink to="/playground">Playground</NuxtLink>
-  </div>
+<div class="prose">
+  <form>
+    <select v-model="locale">
+      <option value="en">en</option>
+      <option value="fr">fr</option>
+    </select>
+    <p>{{ $t("welcome") }}</p>
+    <p>{{ $t("description") }}</p>
+  </form>
+  <NuxtLink to="/playground">Playground</NuxtLink>
+</div>
 </template>
 
 <script setup lang="ts">
