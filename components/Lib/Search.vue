09193c99b286554d492d0f33f349e08ae9cabead
BenoitEirik <olafsergent@gmail.com>
2023-07-26 01:34:11 +0200

    Improve search page

--- components/Lib/Search.vue
+++ components/Lib/Search.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="absolute top-0 left-0 w-screen h-screen z-[1000]" :class="{ 'hidden': !display }"
-  style="backdrop-filter: blur(10px);">
-  COucou
+<div class="absolute top-0 left-0 w-screen h-screen z-[1000] flex flex-col items-stretch"
+  :class="{ '!hidden': !display }" style="backdrop-filter: blur(10px);" @click="escape">
+  <div class="p-4 grow-0 flex justify-center">
+    <AInput ref="searchRef" type="text" :placeholder="$t('Rechercher')" prepend-inner-icon="i-ph-magnifying-glass"
+      class="custom-search-box text-sm" @click.stop="" />
+  </div>
+
+  <div class="grow shrink">
+
+  </div>
 </div>
 </template>
 
@@ -9,9 +16,25 @@
 const { $bus } = useNuxtApp()
 
 const display = ref<boolean>(false)
+const searchRef = ref(null)
 
 $bus.on('openSearch', (isOpening: boolean) => {
   isOpening ? display.value = true : display.value = false
-  console.log(display.value)
 })
+
+const escape = () => {
+  display.value = false
+}
 </script>
+
+/* NOT SCOPED */
+<style lang="scss">
+.custom-search-box {
+  max-width: 400px;
+
+  .a-base-input-input-wrapper {
+    background-color: hsl(var(--a-body-bg-c));
+    border-radius: 999em !important;
+  }
+}
+</style>
